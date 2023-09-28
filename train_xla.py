0cbc68eb5bf7a56c22b2f174d8132cc3d5f9ceb2
alanlomeli <45440937+yisusalanpng@users.noreply.github.com>
2023-09-27 18:12:02 -0600

    update train xla

--- train_xla.py
+++ train_xla.py
@@ -13,107 +13,113 @@ from tqdm import tqdm
 
 from evaluate import evaluate
 from onsets_and_frames import *
-# from onsets_and_frames import constants 
-import onsets_and_frames.constants as constants
 
-#tpu
-import torch_xla.distributed.parallel_loader as pl
+#TPU
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.xla_multiprocessing as xmp
+import multiprocessing as mp
+import torch.distributed as dist
+import torch_xla.distributed.parallel_loader as pl
 
-        
-os.environ['XRT_TPU_CONFIG'] = 'tpu_worker;0;10.0.0.2:8470'
+import torch_xla.experimental.pjrt_backend 
+import torch_xla.experimental.pjrt as pjrt
+
+
+os.environ.pop('TPU_PROCESS_ADDRESSES')
+os.environ.pop('CLOUD_TPU_TASK_ID')
+lock = mp.Manager().Lock()
 
 ex = Experiment('train_transcriber')
 
-
-
-
-def _run():
-
+@ex.config
+def config():
     logdir = constants.LOG_DIR + '/transcriber-' + datetime.now().strftime('%y%m%d-%H%M%S')
-    writer = SummaryWriter(logdir)
-    TRAIN_BATCH_SIZE = 128
-    EPOCHS = 1000000
+    device = constants.DEFAULT_DEVICE
+    iterations = 500000
     resume_iteration = None
-
     checkpoint_interval = 1000
-    
+    train_on = constants.DATASET
+
+    batch_size = 8
     sequence_length = 327680
     model_complexity = 48
-    learning_rate = 0.0006 * xm.xrt_world_size()
+
+    if torch.cuda.is_available() and torch.cuda.get_device_properties(torch.cuda.current_device()).total_memory < 10e9:
+        batch_size //= 2
+        sequence_length //= 2
+        print(f'Reducing batch size to {batch_size} and sequence_length to {sequence_length} to save memory')
+
+    learning_rate = 0.0006
     learning_rate_decay_steps = 10000
     learning_rate_decay_rate = 0.98
+
     leave_one_out = None
+
     clip_gradient_norm = 3
+
     validation_length = sequence_length
     validation_interval = 500
 
-    def train_loop_fn(data_loader, model, optimizer, device, scheduler=None):
-        model.train()
-        loop = tqdm(range(resume_iteration + 1, EPOCHS + 1))
-        for i, batch in zip(loop, cycle(data_loader)):
-
-            optimizer.zero_grad()
-            predictions, losses = model.run_on_batch(batch)
-
-            loss = sum(losses.values())
-
-            loss.backward()
-            xm.optimizer_step(optimizer)  # Use this for TPU optimization
-
-            if clip_gradient_norm:
-                clip_grad_norm_(model.parameters(), clip_gradient_norm)
-
-            if scheduler is not None:
-                scheduler.step()
-
-            for key, value in {'loss': loss, **losses}.items():
-                writer.add_scalar(key, value.item(), global_step=i)
-
-            if i % checkpoint_interval == 0:
-                xm.save(model, os.path.join(logdir, f'model-{i}.pt'))
-                xm.save(optimizer.state_dict(), os.path.join(logdir, 'last-optimizer-state.pt'))
+    ex.observers.append(FileStorageObserver.create(logdir))
 
 
+@ex.automain
+def train(logdir, device, iterations, resume_iteration, checkpoint_interval, train_on, batch_size, sequence_length,
+          model_complexity, learning_rate, learning_rate_decay_steps, learning_rate_decay_rate, leave_one_out,
+          clip_gradient_norm, validation_length, validation_interval):
+
+    os.makedirs(logdir, exist_ok=True)
+    writer = SummaryWriter(logdir)
 
     train_groups, validation_groups = ['train'], ['validation']
 
-    all_years = {'2004', '2006', '2008', '2009', '2011', '2013', '2014', '2015', '2017', '2018'}
-    train_groups = list(all_years)
-    validation_groups = [str(leave_one_out)]
+    if leave_one_out is not None:
+        all_years = {'2004', '2006', '2008', '2009', '2011', '2013', '2014', '2015', '2017', '2018'}
+        train_groups = list(all_years - {str(leave_one_out)})
+        validation_groups = [str(leave_one_out)]
+
     dataset = MAESTRO(groups=train_groups, sequence_length=sequence_length)
 
-    train_data_loader = DataLoader(dataset, TRAIN_BATCH_SIZE, shuffle=True, drop_last=True)
+    xmp.spawn(_mp_fn, args=(logdir, iterations, resume_iteration, checkpoint_interval, dataset, batch_size, model_complexity, learning_rate, learning_rate_decay_steps, learning_rate_decay_rate, clip_gradient_norm, writer), nprocs=8, start_method='fork')
 
 
+def _mp_fn(rank, logdir, iterations, resume_iteration, checkpoint_interval,  dataset, batch_size, model_complexity, learning_rate, learning_rate_decay_steps, learning_rate_decay_rate, clip_gradient_norm, writer):
     device = xm.xla_device()
-
+    dist.init_process_group('xla', init_method='pjrt://')
+    pjrt.broadcast_master_param(model)
     if resume_iteration is None:
         model = OnsetsAndFrames(N_MELS, MAX_MIDI - MIN_MIDI + 1, model_complexity).to(device)
         optimizer = torch.optim.Adam(model.parameters(), learning_rate)
         resume_iteration = 0
     else:
         model_path = os.path.join(logdir, f'../model-{resume_iteration}.pt')
-        model = torch.load(model_path, map_location=device)
+        model = xm.load(model_path, map_location=device)
         optimizer = torch.optim.Adam(model.parameters(), learning_rate)
-        optimizer.load_state_dict(torch.load(os.path.join(logdir, 'last-optimizer-state.pt')))
-
-    model = model.to(device)
-
-
-
-    summary(model)
+        optimizer.load_state_dict(xm.load(os.path.join(logdir, 'last-optimizer-state.pt')))
 
     scheduler = StepLR(optimizer, step_size=learning_rate_decay_steps, gamma=learning_rate_decay_rate)
+    loader = DataLoader(dataset, batch_size, shuffle=True, drop_last=True)
 
-    for epoch in range(EPOCHS):
-        para_loader = pl.ParallelLoader(train_data_loader, [device])
-        train_loop_fn(para_loader.per_device_loader(device), model, optimizer, device, scheduler=scheduler)
+    mp_device_loader = pl.MpDeviceLoader(loader, device)
+
+    loop = tqdm(range(resume_iteration + 1, iterations + 1))
+    for i, batch in zip(loop, cycle(mp_device_loader)):
+        predictions, losses = model.run_on_batch(batch)
+
+        loss = sum(losses.values())
+        optimizer.zero_grad()
+
+        loss.backward()
+        if clip_gradient_norm:
+            clip_grad_norm_(model.parameters(), clip_gradient_norm)
+
+        xm.optimizer_step(optimizer)
+        scheduler.step()
 
 
-def _mp_fn(rank, flags):
-    a = _run()
+        for key, value in {'loss': loss, **losses}.items():
+            writer.add_scalar(key, value.item(), global_step=i)
 
-FLAGS={}
-xmp.spawn(_mp_fn, args=(FLAGS,), nprocs=8, start_method='fork')
+        if i % checkpoint_interval == 0:
+            xm.save(model, os.path.join(logdir, f'model-{i}.pt'))
+            xm.save(optimizer.state_dict(), os.path.join(logdir, 'last-optimizer-state.pt'))
